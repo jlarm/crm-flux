dfdd03d4306ea506cb8be10621e8ea852079e236
jlarm <117611704+jlarm@users.noreply.github.com>
2025-01-29 09:43:32 -0600

    Start of app layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <link rel="shortcut icon" type="image/x-icon" href="{{ asset('favicon.svg') }}" />
+
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net" />
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
@@ -15,24 +17,105 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @fluxStyles
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            <livewire:layout.navigation />
+    <body class="min-h-screen bg-white dark:bg-zinc-800">
+        <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+            <flux:sidebar.toggle class="lg:hidden" icon="bars-2" inset="left" />
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow dark:bg-gray-800">
-                    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <x-application-logo class="mr-4 h-8 w-auto" />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+            <flux:navbar class="-mb-px max-lg:hidden">
+                <flux:navbar.item icon="home" href="#" current>Home</flux:navbar.item>
+                <flux:navbar.item icon="inbox" badge="12" href="#">Dealerships</flux:navbar.item>
+                <flux:navbar.item icon="document-text" href="#">Contacts</flux:navbar.item>
+                <flux:navbar.item icon="calendar" href="#">Reminders</flux:navbar.item>
+                <flux:navbar.item icon="calendar" href="#">Users</flux:navbar.item>
+
+                <flux:separator vertical variant="subtle" class="my-2" />
+
+                <flux:dropdown class="max-lg:hidden">
+                    <flux:navbar.item icon-trailing="chevron-down">Email</flux:navbar.item>
+
+                    <flux:navmenu>
+                        <flux:navmenu.item href="#">Marketing site</flux:navmenu.item>
+                        <flux:navmenu.item href="#">Android app</flux:navmenu.item>
+                        <flux:navmenu.item href="#">Brand guidelines</flux:navmenu.item>
+                    </flux:navmenu>
+                </flux:dropdown>
+            </flux:navbar>
+
+            <flux:spacer />
+
+            <flux:navbar class="mr-4">
+                <flux:navbar.item icon="magnifying-glass" href="#" label="Search" />
+                <flux:navbar.item class="max-lg:hidden" icon="cog-6-tooth" href="#" label="Settings" />
+                <flux:navbar.item class="max-lg:hidden" icon="information-circle" href="#" label="Help" />
+            </flux:navbar>
+
+            <flux:dropdown position="top" align="start">
+                <flux:profile avatar="https://fluxui.dev/img/demo/user.png" />
+
+                <flux:menu>
+                    <flux:menu.radio.group>
+                        <flux:menu.radio checked>Olivia Martin</flux:menu.radio>
+                        <flux:menu.radio>Truly Delta</flux:menu.radio>
+                    </flux:menu.radio.group>
+
+                    <flux:menu.separator />
+
+                    <flux:menu.item icon="arrow-right-start-on-rectangle">Logout</flux:menu.item>
+                </flux:menu>
+            </flux:dropdown>
+        </flux:header>
+
+        <flux:sidebar
+            stashable
+            sticky
+            class="border-r border-zinc-200 bg-zinc-50 lg:hidden dark:border-zinc-700 dark:bg-zinc-900"
+        >
+            <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
+
+            <flux:brand
+                href="#"
+                logo="https://fluxui.dev/img/demo/logo.png"
+                name="Acme Inc."
+                class="px-2 dark:hidden"
+            />
+            <flux:brand
+                href="#"
+                logo="https://fluxui.dev/img/demo/dark-mode-logo.png"
+                name="Acme Inc."
+                class="hidden px-2 dark:flex"
+            />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="home" href="#" current>Home</flux:navlist.item>
+                <flux:navlist.item icon="inbox" badge="12" href="#">Inbox</flux:navlist.item>
+                <flux:navlist.item icon="document-text" href="#">Documents</flux:navlist.item>
+                <flux:navlist.item icon="calendar" href="#">Calendar</flux:navlist.item>
+
+                <flux:navlist.group expandable heading="Favorites" class="max-lg:hidden">
+                    <flux:navlist.item href="#">Marketing site</flux:navlist.item>
+                    <flux:navlist.item href="#">Android app</flux:navlist.item>
+                    <flux:navlist.item href="#">Brand guidelines</flux:navlist.item>
+                </flux:navlist.group>
+            </flux:navlist>
+
+            <flux:spacer />
+
+            <flux:navlist variant="outline">
+                <flux:navlist.item icon="cog-6-tooth" href="#">Settings</flux:navlist.item>
+                <flux:navlist.item icon="information-circle" href="#">Help</flux:navlist.item>
+            </flux:navlist>
+        </flux:sidebar>
+
+        <flux:main container>
+            <flux:heading size="xl" level="1">Good afternoon, Olivia</flux:heading>
+
+            <flux:subheading size="lg" class="mb-6">Here's what's new today</flux:subheading>
+
+            <flux:separator variant="subtle" />
+        </flux:main>
+
         @fluxScripts
     </body>
 </html>
