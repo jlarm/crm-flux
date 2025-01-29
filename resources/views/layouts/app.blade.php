271f11226dfdbe5ac53e367dd0b86544add7be87
jlarm <117611704+jlarm@users.noreply.github.com>
2025-01-29 09:48:30 -0600

    Added navigation component

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,25 +23,7 @@
 
             <x-application-logo class="mr-4 h-8 w-auto" />
 
-            <flux:navbar class="-mb-px max-lg:hidden">
-                <flux:navbar.item icon="home" href="#" current>Home</flux:navbar.item>
-                <flux:navbar.item icon="building-office" href="#">Dealerships</flux:navbar.item>
-                <flux:navbar.item icon="user-group" href="#">Contacts</flux:navbar.item>
-                <flux:navbar.item icon="calendar" href="#">Reminders</flux:navbar.item>
-                <flux:navbar.item icon="user" href="#">Users</flux:navbar.item>
-
-                <flux:separator vertical variant="subtle" class="my-2" />
-
-                <flux:dropdown class="max-lg:hidden">
-                    <flux:navbar.item icon-trailing="chevron-down">Email</flux:navbar.item>
-
-                    <flux:navmenu>
-                        <flux:navmenu.item href="#">Marketing site</flux:navmenu.item>
-                        <flux:navmenu.item href="#">Android app</flux:navmenu.item>
-                        <flux:navmenu.item href="#">Brand guidelines</flux:navmenu.item>
-                    </flux:navmenu>
-                </flux:dropdown>
-            </flux:navbar>
+            <x-navigation />
 
             <flux:spacer />
 
